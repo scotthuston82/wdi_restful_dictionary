6858dd016800eeb68f9a80a5c30f3c6ad7a6cc1f
Scott <scotthuston1982@gmail.com>
2018-01-18 15:09:36 -0500

    finished part 2

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,31 +1,52 @@
 class EntriesController < ApplicationController
 
   def index
-
+    @entries = Entry.all
   end
 
   def show
-
+    @entry = Entry.find(params[:id])
   end
 
   def new
+    @entry = Entry.new
     render :new
   end
 
   def create
-    redirect_to entries_url
+    @entry = Entry.new
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @entry = Entry.find(params[:id])
   end
 
   def update
-    redirect_to entry_url(params[:id])
+    @entry = Entry.new
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entry_url(params[:id])
+    else
+      render :new
+    end
   end
 
   def destroy
-    redirect_to index_url
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_url
   end
 
 end
